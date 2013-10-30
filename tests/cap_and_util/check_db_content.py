a3ce2dde0ae82cbb2bcbd849be8cae3a76a001f1
Wade Adkins <wadkins@redhat.com>
2013-10-30 11:19:39 -0400

    WIP

--- tests/cap_and_util/check_db_content.py
+++ tests/cap_and_util/check_db_content.py
@@ -1,16 +1,36 @@
-import datetime
 import db
+
 
 def check_off_hours(db_session, resource_id, columns, hours_to_check):
 	resource_id = resource_id
 	columns = columns
-	past_date = past_date
-	hours_off = hours_off
-
+	hours_to_check = hours_to_check
 	session = db_session
 
-	entry = session.query(db.MetricRollup).filter(db.MetricRollup.resource_id == resource_id, db.MetricRollup.timestamp == '2013-10-17 12:00:00').first()
 
-	entry.disk_usage_rate_average
+	#entry = session.query(db.MetricRollup).filter(db.MetricRollup.resource_id == resource_id, db.MetricRollup.timestamp == '2013-10-29 14:00:00').first()
+
+	i=0
+	j=0
+
+	while(i < len(hours_to_check)):
+
+		temp_entry = session.query(db.MetricRollup).filter(db.MetricRollup.resource_id == resource_id, db.MetricRollup.timestamp == hours_to_check[i]).first()
+
+		#assert 0
+
+		while(j < len(columns)):
+
+			temp_value = getattr(temp_entry, columns[j])
+
+			print('\nThe Resource Name is: %s and the timestamp is: %s  The value in column: %s is %f' %(temp_entry.resource_name, hours_to_check[i], columns[j], temp_value))
+
+			j += 1
+
+		i += 1
+
+	#assert 0
+
+	#return entry.cpu_usage_rate_average
 
 
--- tests/cap_and_util/test_c_and_u_collection.py
+++ tests/cap_and_util/test_c_and_u_collection.py
@@ -1,40 +1,52 @@
 import pytest
-import datetime
 import collections
 from unittestzero import Assert
 from check_db_content import check_off_hours
 from selenium.webdriver.common.keys import Keys
 
+
 @pytest.fixture
-def vm_info(db_session):
+def vm_info(db_session, vm_name):
     import db
     session = db_session
-    vm_info = session.query(db.Vm).filter_by(name = 'wadkins-cfme-35-1').first()
+    vm_info = session.query(db.Vm).filter_by(name= vm_name ).first()
     resource_id = vm_info.id
     name = vm_info.name
     vm_info = collections.namedtuple('vm_info', ['id', 'name'])
     vm_info = vm_info(resource_id, name)
     return vm_info
 
+
 @pytest.mark.nondestructive
 @pytest.mark.usefixtures("maximized")
-def test_gaps_in_collectoin(vm_info):
+def test_gaps_in_collectoin(db_session):
 
-	hours_to_check = {
-		'2013-10-17 10:00:00',
-	}
+    i = 0
+    vms_to_check = [
+        'wadkins-cfme-35-1',
+        'wadkins-cfme-520-37-1021-2'
+    ]
 
-	columns = {
-	'disk_usage_rate_average'
-	}    
+    while (i < len(vms_to_check)):
+        temp_vm = vm_info(db_session, vms_to_check[i])
+        resource_id = temp_vm.id
 
-    while(past_date <= date):
+        hours_to_check = [
+            '2013-10-29 14:00:00',
+            '2013-10-29 18:00:00',
+            '2013-10-30 12:00:00 '
+        ]
 
-    	result = check_off_hours(db_session, resource_id, columns, hours_to_check)
+        columns = [
+            'cpu_usage_rate_average',
+            'disk_usage_rate_average'
+        ]   
 
-    	assert 0
+        result = check_off_hours(db_session, resource_id, columns, hours_to_check)
 
-    	past_date = past_date + datetime.timedelta(hours = 2)
+        i += 1
+
+    #assert 0
 
 
 
